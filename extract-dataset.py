e5a00dd5503c815774819beb43507fbd4e1ed33e
Tarindu Jayatilaka <tarindujayatilaka@gmail.com>
2021-07-22 16:45:59 +0530

    add code feature and label extraction script

---
+++ extract-dataset.py
@@ -0,0 +1,235 @@
+import json
+import multiprocessing
+import subprocess
+import os
+import tqdm
+import logging
+
+debug_file_path = os.path.join(os.path.dirname(os.getcwd()), 'debug.log')
+logging.basicConfig(filename=debug_file_path, filemode='w+', level=logging.DEBUG)
+
+# provide in increasing order of level of optimization
+opt_levels = ['O1', 'O2', 'O3']
+
+llvm_build_path = '/Users/tarindujayatilaka/Documents/LLVM/llvm-build'
+llvm_extract_path = os.path.join(llvm_build_path, 'bin', 'llvm-extract')
+llc_path = os.path.join(llvm_build_path, 'bin', 'llc')
+
+"""
+takes a json object as input and returns a list of commands to create .ll files for each optimization level, and dump code features.
+
+Input:  {
+            "directory": ..,
+            "command": ..,
+            "file": ..
+        }
+
+Output: {
+            'O1':               [command, output_dir],
+            'O2':               [command, output_dir],
+            'O3':               [command, output_dir],
+            'code-features':    [command, output_path]
+        }
+
+"""
+def get_data_dump_commands(obj):
+    cmd = obj['command']
+    cmd_list = cmd.split()
+    
+    # use -o and -O1 flags as anchors to modify the command
+    output_anchor = cmd_list.index('-o')
+    opt_anchor = cmd_list.index('-O1')
+    
+    cmd_list.insert(output_anchor, '-emit-llvm')
+    
+    prev_output_path = cmd_list[output_anchor+2]
+
+    commands = {}
+    # change output path and optimization level
+    for opt_level in opt_levels:
+        cmd_list_opt_level = cmd_list.copy()
+        output_path = os.path.join(os.path.dirname(os.getcwd()), f'{opt_level}-ll{os.path.sep}', prev_output_path[:-1] + 'll')
+        cmd_list_opt_level[output_anchor+2] = output_path
+        cmd_list_opt_level[opt_anchor] = '-' + opt_level
+        output_dir = output_path[:output_path.rfind(os.path.sep)]
+        commands[opt_level] = [cmd_list_opt_level, output_dir]
+    
+    #command to dump code features
+    code_feature_dump_path = os.path.abspath(os.path.join(os.path.dirname(os.getcwd()), f'code-features{os.path.sep}', prev_output_path[:-1] + 'txt'))
+    code_feature_dump_command = commands[opt_levels[0]][0].copy()
+    code_feature_dump_command.insert(opt_anchor, f'-mlpm-feature-dump-path={code_feature_dump_path}')
+    code_feature_dump_command.insert(opt_anchor, '-mllvm')
+    code_feature_dump_command.insert(opt_anchor, '-dump-mlpm-data')
+    code_feature_dump_command.insert(opt_anchor, '-mllvm')
+    commands['code-features'] = [code_feature_dump_command, code_feature_dump_path]
+
+    return commands
+
+def run_data_dump_commands(command_output_dir_dict):
+    #crete directories
+    output_path = command_output_dir_dict['code-features'][1]
+    output_dir = output_path[:output_path.rfind(os.path.sep)]
+    logging.debug(output_path + '\n')
+    subprocess.run(['mkdir', '-p', output_dir], check=True)
+
+    for opt_level in opt_levels: 
+        output_dir = command_output_dir_dict[opt_level][1]
+        logging.debug('mkdir -p ' + output_dir +'\n')
+        subprocess.run(['mkdir', '-p', output_dir], check=True)
+
+    # run the code feature dump command first (needs to replace function annotations creted by data dump by running O1 again)
+    command = command_output_dir_dict['code-features'][0]
+    logging.debug(" ".join(command) + '\n')
+    subprocess.run(command, check=True)
+
+    # create the .ll files for all optimization levels
+    for opt_level in opt_levels: 
+        command = command_output_dir_dict[opt_level][0]
+        logging.debug(" ".join(command) + '\n')
+        subprocess.run(command, check=True) 
+
+"""
+compares the assembly files and returns the minimum optimization level for function
+
+module_dir_paths =  {
+                        'O1':               [module_path, module_sub_dir],
+                        'O2':               [module_path, module_sub_dir],
+                        'O3':               [module_path, module_sub_dir]
+                    }
+
+"""
+def get_optimization_level_label(function, module_dir_paths):
+    asm_files = []
+
+    for opt_level in opt_levels: 
+        module_dir_path = module_dir_paths[opt_level]
+        module_path = module_dir_path[0]
+        ir_output_path = os.path.join(module_dir_path[1], function + '.ll')
+        as_output_path = os.path.join(module_dir_path[1], function + '.s')
+        
+        command = [llvm_extract_path, '-S', '-func', function, module_path, '-o', ir_output_path]
+        logging.debug(" ".join(command) + '\n')
+        try:
+            subprocess.run(command, check=True, stderr=subprocess.DEVNULL)
+
+            command = [llc_path, ir_output_path, '-o', as_output_path]
+            logging.debug(" ".join(command) + '\n')
+            subprocess.run(command, check=True)
+
+            with open(as_output_path, 'r+') as f:
+                asm_files.append(f.read())
+        
+        # flag asm file as -1 if function not found
+        except subprocess.CalledProcessError as e:
+            logging.debug(e)
+            asm_files.append(-1)
+
+
+    ## note: in case of multiple optimization levels, replace the following logic with performance
+    ## for now, assume the performance always increase with the optimization level
+
+    # if function not found, assign the lowest optimization level without the function
+    opt_level_label = ''
+    for i in range(len(opt_levels)):
+        if asm_files[i] == -1:
+            opt_level_label += opt_levels[i] + '-'
+            
+    if (opt_level_label):
+         opt_level_label += 'FNF'
+    # if all of the optimization levels had the function, assign the label as follows
+    else:
+    # start from the end and compare adjacent optimization levels
+    # if two adjacents optimization levels produce the same asm, assign the lower optimization level
+        index = len(opt_levels) - 1
+        opt_level_label = opt_levels[index]
+        for _ in range(len(opt_levels) - 1):
+            if asm_files[index] == asm_files[index-1]:
+                index = index-1
+                opt_level_label = opt_levels[index]
+            else:
+                break
+
+    return opt_level_label
+
+# takes a module and returns a csv file containing training features and labels
+def get_training_dataset(command_output_dir_dict):
+    code_features_path = command_output_dir_dict['code-features'][1]
+    module_name = code_features_path[code_features_path.rfind('/') + 1 : code_features_path.rfind('.')]
+
+    # create sub directories for the module
+    module_dir_paths = {}
+    for opt_level in opt_levels: 
+        parent_dir = command_output_dir_dict[opt_level][1]
+        module_path = os.path.join(parent_dir, module_name + '.ll')
+        module_sub_dir = os.path.join(parent_dir, module_name + '.ll.dir')
+        logging.debug('mkdir -p ' + module_sub_dir +'\n')
+        subprocess.run(['mkdir', '-p', module_sub_dir], check=True)
+        module_dir_paths[opt_level] = [module_path, module_sub_dir]
+
+    with open(code_features_path, 'r+') as f:
+        function_code_features_list = list(map(str.strip, list(filter(None, f.read().split('####')))))
+
+    dataset = ''
+
+    # creates a row in the format: function, [code features], optimization level
+    for function_code_features in function_code_features_list:
+        function_code_features = function_code_features.split('\n')
+        
+        # index
+        function = function_code_features[0]
+        
+        # code features list
+        features = ''
+        for code_feature in function_code_features[1:]:
+            features += code_feature.split(':')[-1] + ','
+        features = features[:-1]
+
+        # get the training label
+        label = get_optimization_level_label(function, module_dir_paths)
+        
+        row = function + ',' + features + ', ' + label
+        dataset += row + '\n'
+    
+    return dataset
+
+if __name__ == '__main__':  
+    with open('../O1-build/compile_commands.json', 'r+') as f:
+        data = json.load(f)
+
+    num_workers = multiprocessing.cpu_count()
+
+    # FIX ME: remove this later
+    data = data[:4]
+
+    with multiprocessing.Pool(num_workers) as pool:
+        command_output_dir_dict_list = list(tqdm.tqdm(pool.imap(get_data_dump_commands, data), total=len(data)))
+
+    with multiprocessing.Pool(num_workers) as pool:
+        list(tqdm.tqdm(pool.imap(run_data_dump_commands, command_output_dir_dict_list), total=len(command_output_dir_dict_list)))
+
+    with multiprocessing.Pool(num_workers) as pool:
+        with open('../dataset.csv', 'w+') as f:
+            for sub_dataset in list(tqdm.tqdm(pool.imap(get_training_dataset, command_output_dir_dict_list), total=len(command_output_dir_dict_list))):
+                f.write(sub_dataset)
+
+
+
+    # with open('results.txt', 'w') as f:
+    #     for result in p.imap(mp_worker, filenames):
+    #         # (filename, count) tuples from worker
+    #         f.write('%s: %d\n' % result)
+
+    # print(subprocess.run(['ls']))
+    # subprocess.run(cmd_list, check=True)
+    # command = " ".join(cmd_list)
+    # print(command)
+
+    # with open('../O1-build/compile_commands.json', 'w+') as f:
+    #     json.dump(modified_data, f, indent=4)
+        
+
+# import csv   
+# fields=['first','second','third']
+# with open(r'name', 'a') as f:
+#     writer = csv.writer(f)
+#     writer.writerow(fields)
